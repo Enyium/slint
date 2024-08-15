59ba6402f56d0f6e6d59fec05231e5a979d699e8
Simon Hausmann <simon.hausmann@slint.dev>
2024-08-15 15:37:11 +0200

    STM32: Avoid the use of a generated header file
    The generated header makes it impossible to include this interface library in a BSP-independent cmake package. Instead, determine the BSP header files to include at pre-processing time.

--- api/cpp/stm32/slint-stm.h
+++ api/cpp/stm32/slint-stm.h
@@ -3,8 +3,20 @@
 
 #pragma once
 
+#define STRINGIFY(X) STRINGIFY2(X)
+#define STRINGIFY2(X) #X
+#define CAT(X, Y) CAT2(X, Y)
+#define CAT2(X, Y) X##Y
+
 #include <slint-platform.h>
-#include <slint-stm-config.h>
+
+#if !defined(SLINT_STM32_BSP_NAME)
+#    error "Please define the SLINT_STM32_BSP_NAME pre-processor macro to the base name of the BSP, without quotes, such as SLINT_STM32_BSP_NAME=stm32h747i_disco"
+#endif
+
+#include STRINGIFY(SLINT_STM32_BSP_NAME.h)
+#include STRINGIFY(CAT(SLINT_STM32_BSP_NAME, _lcd.h))
+#include STRINGIFY(CAT(SLINT_STM32_BSP_NAME, _ts.h))
 
 struct SlintPlatformConfiguration
 {
